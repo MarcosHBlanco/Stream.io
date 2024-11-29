dcc516595f0cdf9c895825b28d42bec2cdb6d8f0
MarcosHBlanco <124410355+MarcosHBlanco@users.noreply.github.com>
2024-11-28 18:24:27 -0800

    First commit for Stream.io project

---
+++ script.js
@@ -0,0 +1,36 @@
+const carouselContainer = document.getElementById("carrousel2");
+const carouselItems = document.querySelectorAll(".carrousel-item2");
+let currentIndex = 0;
+// Set the first item as active on page load
+function setActive() {
+	carouselItems[currentIndex].classList.add("carrousel-item-active");
+}
+
+// Scroll to the next item
+function changeActiveNext() {
+	carouselItems[currentIndex].classList.remove("carrousel-item-active");
+	currentIndex = (currentIndex + 1) % carouselItems.length;
+	carouselItems[currentIndex].classList.add("carrousel-item-active");
+	ensureVisible(currentIndex);
+}
+
+// Scroll to the previous item
+function changeActivePrev() {
+	carouselItems[currentIndex].classList.remove("carrousel-item-active");
+	currentIndex =
+		(currentIndex - 1 + carouselItems.length) % carouselItems.length;
+	carouselItems[currentIndex].classList.add("carrousel-item-active");
+	ensureVisible(currentIndex);
+}
+
+// Ensure the active item is visible
+function ensureVisible(index) {
+	carouselItems[index].scrollIntoView({
+		behavior: "smooth",
+		block: "nearest",
+		inline: "center", // Keeps the active item in the center of the carousel
+	});
+}
+
+// Initialize the active item
+window.onload = setActive;
